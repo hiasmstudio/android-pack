eac404c0445896099620abe9e1df7675c9759f35
Ivan Sadovyi <ivan.sadovyi@teamvoy.com>
2016-08-18 18:10:15 +0300

    Ask question

--- code/result/MainActivity.java
+++ code/result/MainActivity.java
@@ -6,6 +6,7 @@ import android.widget.Toast;
 public class MainActivity extends HiActivity {
 	@Override
 	protected void onCreate() {
+Toast.makeText(getHiContext(), "123", Toast.LENGTH_SHORT).show();
 	}
 	@Override
 	protected void onDestroy() {
